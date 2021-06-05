fe6d4ca185cdba4cfb858df9b19165ed7dde0463
Alexander Nittka <alex@nittka.de>
2021-06-05 18:22:32 +0200

    prevent single line comment toggling
    fixes #15

--- org.tvtower.db.ui/src/org/tvtower/db/ui/DatabaseUiModule.java
+++ org.tvtower.db.ui/src/org/tvtower/db/ui/DatabaseUiModule.java
@@ -7,9 +7,13 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.xtext.ui.editor.hover.IEObjectHover;
 import org.eclipse.xtext.ui.editor.hover.IEObjectHoverProvider;
 import org.eclipse.xtext.ui.editor.outline.actions.OutlineWithEditorLinker;
+import org.eclipse.xtext.ui.editor.toggleComments.DefaultSingleLineCommentHelper;
 import org.tvtower.db.ui.hover.DatabaseEObjectHover;
 import org.tvtower.db.ui.hover.DatabaseHoverProvider;
 import org.tvtower.db.ui.outline.DatabaseOutlineWithEditorLinker;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 /**
  * Use this class to register components to be used within the Eclipse IDE.
@@ -30,6 +34,14 @@ public class DatabaseUiModule extends AbstractDatabaseUiModule {
 
 	@Override
 	public Class<? extends IEObjectHover> bindIEObjectHover() {
-		return  DatabaseEObjectHover.class;
+		return DatabaseEObjectHover.class;
+	}
+
+	@Override
+	public void configure(Binder binder) {
+		super.configure(binder);
+		//cause comment rule not to be found, so that no comment toggling is done
+		binder.bind(String.class).annotatedWith(Names.named(DefaultSingleLineCommentHelper.SL_COMMENT))
+				.toInstance("thereIsNoSLComment");
 	}
 }
