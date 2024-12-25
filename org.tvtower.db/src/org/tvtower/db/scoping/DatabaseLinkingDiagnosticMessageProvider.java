1d176f21df600929ae1c6725e7feb538a6f0ed58
Alexander Nittka <alex@nittka.de>
2024-12-25 09:40:13 +0100

    support preselected cast syntax

--- org.tvtower.db/src/org/tvtower/db/scoping/DatabaseLinkingDiagnosticMessageProvider.java
+++ org.tvtower.db/src/org/tvtower/db/scoping/DatabaseLinkingDiagnosticMessageProvider.java
@@ -2,14 +2,17 @@ package org.tvtower.db.scoping;
 
 import org.eclipse.xtext.diagnostics.DiagnosticMessage;
 import org.eclipse.xtext.linking.impl.LinkingDiagnosticMessageProvider;
+import org.tvtower.db.database.DatabasePackage;
 import org.tvtower.db.database.Effect;
 
 public class DatabaseLinkingDiagnosticMessageProvider extends LinkingDiagnosticMessageProvider {
 
+	private static DatabasePackage $ = DatabasePackage.eINSTANCE;
+
 	@Override
 	public DiagnosticMessage getUnresolvedProxyMessage(ILinkingDiagnosticContext context) {
-		// do not try to resolve variables within effects
-		if (context.getContext() instanceof Effect) {
+		// do not try to resolve variables within effects or preselected cast
+		if (context.getContext() instanceof Effect || context.getReference() == $.getJob_PreselectedCast()) {
 			if (context.getLinkText() != null && context.getLinkText().matches("\\$\\{\\w+\\}")) {
 				return null;
 			}
